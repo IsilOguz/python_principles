ab386e3de48d1f17d54c5c745b46132faa53b1fe
IsilOguz <isiloguz1014@gmail.com>
2022-02-03 20:54:45 +0300

    20 edited

--- python_principles/20_custom_zip.py
+++ python_principles/20_custom_zip.py
@@ -20,5 +20,24 @@ Should return:
  (3, 8)]
 """
 
+# ugly but understandable solution
+def zap(a, b):
+    result = []
+    for i in range(len(a)):
+        item_from_a = a[i]
+        item_from_b = b[i]
+        tup = (item_from_a, item_from_b)
+        result.append(tup)
+    return result
+
+# concise solution with list comprehensions
+def zap(a, b):
+    return [(a[i], b[i]) for i in range(len(a))]
+
+# my solution
 def zap(a,b):
-    pass
+    myList = []
+    for i in range(len(a)):
+        myTuple = (a[i],b[i])
+        myList.append(myTuple)
+    return myList
